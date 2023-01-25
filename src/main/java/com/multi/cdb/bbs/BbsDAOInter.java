66e19f6dac7998e2af2f3ff4f1d52aae2a6cfc95
tandk97 <113963022+tandk97@users.noreply.github.com>
2023-01-25 21:07:12 +0900

    Delete src directory

--- src/main/java/com/multi/cdb/bbs/BbsDAOInter.java
+++
@@ -1,9 +0,0 @@
-package com.multi.cdb.bbs;
-
-import java.util.List;
-
-public interface BbsDAOInter {
-
-	List<BbsVO> list(); 
-
-}
--- src/main/java/com/multi/cdb/bbs/BbsServiceInter.java
+++
@@ -1,9 +0,0 @@
-package com.multi.cdb.bbs;
-
-import java.util.List;
-
-public interface BbsServiceInter {
-
-	List<BbsVO> list();
-
-}
