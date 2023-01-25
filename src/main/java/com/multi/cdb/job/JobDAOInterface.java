109f23d76612f488a181c3b4456578ae93daefac
SongSeoYu <113963126+SongSeoYu@users.noreply.github.com>
2023-01-25 17:20:29 +0900

    ver2
    ver2

--- src/main/java/com/multi/cdb/job/JobDAOInterface.java
+++ src/main/java/com/multi/cdb/job/JobDAOInterface.java
@@ -1,5 +1,26 @@
 package com.multi.cdb.job;
 
-public interface JobDAOInterface extends Interface {
+import java.util.List;
 
+public interface JobDAOInterface {
+	// ����
+		void insert(JobVO vo);
+		void getApiDataYouth(YouthJobVO vo);
+
+		// ����
+		void update(JobVO vo);
+
+		// �󼼰˻�
+		List<JobVO> one(String job_wantedAuthNo);
+		List<YouthJobVO> details(String job_wantedAuthNo);
+
+		//������ ���� ��ü ����Ʈ
+
+		List<JobVO> getJobByCri(Criteria cri);
+		List<YouthJobVO> getYouthJobByCri(Criteria cri);
+		
+		int getCountJobByCri(Criteria cri);
+		int getCountYouthJobByCri(Criteria cri);
+
+		
 }
--- src/main/java/com/multi/cdb/job/JobServiceInterface.java
+++ src/main/java/com/multi/cdb/job/JobServiceInterface.java
@@ -7,18 +7,23 @@ import org.springframework.ui.Model;
 public interface JobServiceInterface {
 
 	public void insert(JobVO vo);
+	public void getApiDataYouth(YouthJobVO vo);
 
 	public void update(JobVO vo);
 	
 	public List<JobVO> one(String job_wantedAuthNo);
+	public List<YouthJobVO> details(String job_wantedAuthNo);
 	
 	//������ ���� ��ü����Ʈ
-	public List<JobVO> all();
 	
-	
-	public List<JobVO> getAllList();
+	/*
+	 * public List<JobVO> getAllList(); public List<YouthJobVO>
+	 * getAllYouthJobList();
+	 */
 
 	public List<JobVO> getJobByCri(Criteria cri);
+	public List<YouthJobVO> getYouthJobByCri(Criteria cri);
 
-	
+	public int getCountJobByCri(Criteria cri);
+	public int getCountYouthJobByCri(Criteria cri);
 }
